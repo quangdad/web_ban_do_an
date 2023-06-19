0727eec7431a4614313fb53a915ff9b604119ce6
Bui Huu Dat <dat54261001@gmail.com>
2023-06-19 16:26:46 +0700

    update url server

--- src/components/common/uploadImage/index.js
+++ src/components/common/uploadImage/index.js
@@ -4,7 +4,7 @@ import { dnrUri } from "../../../api/axiosClient";
 export const imageUpload = async (img) => {
   const data = new FormData();
   data.append("file", img);
-  data.append("upload_preset", "webbandoan");
+  data.append("upload_preset", "webbanhang");
   data.append("clound_name", "dhotuzjtr");
   const image = await axios.post(dnrUri, data);
   return image.data.url;
