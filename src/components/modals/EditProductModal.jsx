0890383799f6a5f38323a1a05547252a2f49668f
Bui Huu Dat <dat54261001@gmail.com>
2023-06-19 16:33:12 +0700

    update url server

--- src/components/modals/EditProductModal.jsx
+++ src/components/modals/EditProductModal.jsx
@@ -28,6 +28,8 @@ const style = {
   left: "50%",
   transform: "translate(-50%, -50%)",
   width: 500,
+  height: 700,
+  overflow: "auto",
   bgcolor: "background.paper",
   boxShadow: 24,
   p: 4,
