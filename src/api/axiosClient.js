5ab652dbb6995b643d3f98c01124eab7aa705873
Bui Huu Dat <dat54261001@gmail.com>
2023-06-19 16:04:11 +0700

    update url server

--- src/api/axiosClient.js
+++ src/api/axiosClient.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 import queryString from "query-string";
 
-const baseURL = "http://localhost:9000/api/";
+// const baseURL = "http://localhost:9000/api/";
+const baseURL = "https://be-ban-do-an2.onrender.com/api/";
 export const URL_SV2 = "http://localhost:9000/api/v1";
 export const dnrUri = "https://api.cloudinary.com/v1_1/dhotuzjtr/upload";
 
