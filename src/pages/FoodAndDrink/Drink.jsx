47d03a760799b27f57670b1895fabbc1a22a36cd
Bui Huu Dat <dat54261001@gmail.com>
2023-06-22 12:37:46 +0700

    fix somthing bugs

--- src/pages/FoodAndDrink/Drink.jsx
+++ src/pages/FoodAndDrink/Drink.jsx
@@ -1,4 +1,4 @@
-import { Box } from "@mui/material";
+import { Box, LinearProgress } from "@mui/material";
 import React from "react";
 import { useEffect } from "react";
 import { useState } from "react";
@@ -8,19 +8,28 @@ import { useSelector } from "react-redux";
 
 const Drink = () => {
   const [products, setProducts] = useState([]);
+  const [loading, setLoading] = useState(true);
 
   const productsData = useSelector((state) => state.products.data);
 
   useEffect(() => {
     const getProducts = () => {
-      setProducts(
-        _.filter(productsData, (e) => e.type.split("/")[0] === "drink")
-      );
+      try {
+        setProducts(
+          _.filter(productsData, (e) => e.type.split("/")[0] === "drink")
+        );
+      } catch (e) {
+        console.log(e);
+      } finally {
+        setLoading(false);
+      }
     };
     getProducts();
   }, [productsData]);
 
-  return (
+  return loading ? (
+    <LinearProgress />
+  ) : (
     <Box minHeight={"100vh"}>
       {products && <Tabsbar header={1} products={products} />}
     </Box>
--- src/pages/FoodAndDrink/Food.jsx
+++ src/pages/FoodAndDrink/Food.jsx
@@ -1,4 +1,4 @@
-import { Box } from "@mui/material";
+import { Box, LinearProgress } from "@mui/material";
 import React from "react";
 import { useSelector } from "react-redux";
 import Tabsbar from "../../components/Tabs";
@@ -8,18 +8,27 @@ import { useEffect } from "react";
 
 const Food = () => {
   const [products, setProducts] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
   const productsData = useSelector((state) => state.products.data);
 
   useEffect(() => {
     const getProduct = () => {
-      setProducts(
-        _.filter(productsData, (e) => e.type.split("/")[0] === "food")
-      );
+      try {
+        setProducts(
+          _.filter(productsData, (e) => e.type.split("/")[0] === "food")
+        );
+      } catch (e) {
+        console.log(e);
+      } finally {
+        setIsLoading(false);
+      }
     };
     getProduct();
   }, [productsData]);
 
-  return (
+  return isLoading ? (
+    <LinearProgress />
+  ) : (
     <Box minHeight={"100vh"}>
       {products && <Tabsbar header={0} products={products} />}
     </Box>
